a550c9af2f98cf1ba29b00f9dc5f20a257c53af7
Paulo Rozatto <rozatto.paulo.victor@gmail.com>
2022-08-07 19:13:49 -0300

    mesuring execution time

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <list>
 #include <stack>
+#include <chrono>
+#include <time.h>
 
 #include "search.h"
 
@@ -9,6 +11,8 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+    std::chrono::duration<double> elapsed_seconds;
     ifstream input("input.txt");
     char opt = '\0';
     bool has_solution;
@@ -45,25 +49,37 @@ int main(int argc, char const *argv[])
         switch (opt)
         {
         case '1':
+            start = std::chrono::system_clock::now();
             has_solution = backtracking(root.makeChildCopy(), &path);
+            end = std::chrono::system_clock::now();
             break;
         case '2':
+            start = std::chrono::system_clock::now();
             has_solution = bfs(&root, &path);
+            end = std::chrono::system_clock::now();
             break;
         case '3':
             int max_depth;
             cout << "Profundidade maxima: ";
             cin >> max_depth;
+            start = std::chrono::system_clock::now();
             has_solution = dfs(&root, &path, max_depth);
+            end = std::chrono::system_clock::now();
             break;
         case '4':
+            start = std::chrono::system_clock::now();
             has_solution = greed(&root, &path);
+            end = std::chrono::system_clock::now();
             break;
         case '5':
+            start = std::chrono::system_clock::now();
             has_solution = aStar(&root, &path);
+            end = std::chrono::system_clock::now();
             break;
         case '6':
+            start = std::chrono::system_clock::now();
             has_solution = idaStar(root.makeChildCopy(), &path);
+            end = std::chrono::system_clock::now();
             break;
         case '0':
             cout << "Encerrando" << endl;
@@ -80,9 +96,11 @@ int main(int argc, char const *argv[])
         else
             cout << "Sem solucao" << endl;
 
+        elapsed_seconds = end - start;
         cout << "Profundidade: " << depth << endl
              << "Nos visitados: " << node_count << endl
-             << "Ramificacao: " << ((node_count - 1) / (float)nonleaf_count) << endl;
+             << "Ramificacao: " << ((node_count - 1) / (float)nonleaf_count) << endl
+             << "Tempo: " << elapsed_seconds.count() << "s" << endl;
 
         freeList(&path);
     }
